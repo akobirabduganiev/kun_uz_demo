9c718e432ce710b18d1bbc80dafbc92edd15863f
Akobir <akobir.abduganiev@ya.ru>
2022-05-10 11:16:02 +0500

    Validation

--- src/main/java/com/company/dto/AuthDTO.java
+++ src/main/java/com/company/dto/AuthDTO.java
@@ -3,9 +3,13 @@ package com.company.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Setter
 @Getter
 public class AuthDTO {
+    @NotNull
     private String email;
+    @NotNull
     private String password;
 }
